8ee8428b225d1a61c1f3a4f00f20fe6ea57a2359
dare2python <31766642+Dare2python@users.noreply.github.com>
2019-11-02 11:57:14 +0200

    refactor main function

---
+++ simulator_function.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+
+def function(x):
+    return np.sin(x)
--- visualize.py
+++ visualize.py
@@ -1,11 +1,12 @@
 # importing the required modules
 import matplotlib.pyplot as plt
 import numpy as np
+import simulator_function as f
 
 # setting the x - coordinates
-x = np.arange(0, 2 * (np.pi), 0.1)
+x = np.arange(0, 2 * np.pi, 0.1)
 # setting the corresponding y - coordinates
-y = np.sin(x)
+y = f.function(x)
 
 # potting the points
 plt.plot(x, y)
