2f8ddef9ea44800c189ab5faa9669827bf10f068
dare2python <31766642+Dare2python@users.noreply.github.com>
2019-11-03 11:46:17 +0200

    make Gaussian

--- simulator_function.py
+++ simulator_function.py
@@ -1,5 +1,20 @@
 import numpy as np
 
 
-def function(x):
-    return np.sin(x)
+#  https://docs.scipy.org/doc/numpy-1.15.0/reference/generated/
+#  numpy.random.normal.html
+#  https://en.wikipedia.org/wiki/Gaussian_function
+#  https://en.wikipedia.org/wiki/Normal_distribution
+def makeGaussian(mu=14, step=1, sigma=3):
+    """ Make gaussian.
+    mu is the mean value
+    step is an interval for dot plotting
+    sigma is the standard deviation.
+    """
+
+    x = np.arange(mu - 3*sigma, mu + 3*sigma, step, float)
+
+    y = 1/(sigma * np.sqrt(2 * np.pi)) \
+        * np.exp(- (x - mu)**2 / (2 * sigma**2))
+
+    return x, y
--- visualize.py
+++ visualize.py
@@ -4,12 +4,15 @@ import numpy as np
 import simulator_function as f
 
 # setting the x - coordinates
-x = np.arange(0, 2 * np.pi, 0.1)
+mu = 14
+step = 0.1
+sigma = 3
+
 # setting the corresponding y - coordinates
-y = f.function(x)
+function = f.makeGaussian(mu, step, sigma)
 
 # potting the points
-plt.plot(x, y)
+plt.plot(*function)
 
 # function to show the plot
 plt.show()
